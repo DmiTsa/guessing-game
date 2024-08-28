f7a601d4ee310efb0d3055998a2acc652c919738
DmiTsa <dmitrytaskaev.dt@gmail.com>
2024-08-28 22:08:48 +0300

    feat: game guess binary

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.left = 0
+        this.right = 0
+        this.current = 0
+    }
 
     setRange(min, max) {
-
+        this.left = min
+        this.right = max
     }
 
     guess() {
-
+        this.current = Math.round((this.left + this.right) / 2)
+        return this.current
     }
 
     lower() {
-
+        this.right = this.current
     }
 
     greater() {
-
+        this.left = this.current
     }
 }
 
